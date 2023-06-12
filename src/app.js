0cb897c96fbe2ea17b828deb185fab9fc96456c5
Alexander-sys18 <119635571+Alexander-sys18@users.noreply.github.com>
2023-06-12 04:04:21 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let pronouns = ['the','our'];
+let adjectives = ['great', 'big'];
+let nouns = ['jogger','racoon'];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+for (let i = 0; i < pronouns.length; i++) {
+  for (let j = 0; j < adjectives.length; j++) {
+    for (let k = 0; k < nouns.length; k++) {
+      console.log(pronouns[i] + adjectives[j] + nouns[k] + '.com');
+    }
+  }
+}
